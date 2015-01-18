204f4a080f38b7a301b183cb99c850353a1c9adb
Jason Storey <jason.andrew.storey@gmail.com>
2015-01-18 16:18:12 +0000

    Remove unnecessary spies from test.

--- src/test/java/com/jasonstorey/maunbot/components/TwitterInstructionSourceTest.java
+++ src/test/java/com/jasonstorey/maunbot/components/TwitterInstructionSourceTest.java
@@ -1,7 +1,6 @@
 package com.jasonstorey.maunbot.components;
 
 import com.jasonstorey.maunbot.Maunbot;
-import com.jasonstorey.maunbot.Voice;
 import com.jasonstorey.maunbot.model.Instruction;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,29 +17,27 @@ public class TwitterInstructionSourceTest {
     @Before
     public void setUp() throws Exception {
         twitterInstructionSource = new TwitterInstructionSource();
-        maunbot = new Maunbot(mock(Voice.class));
+        maunbot = mock(Maunbot.class);
         status = mock(Status.class);
     }
 
     @Test
     public void onStatus_withValidInstruction_observerIsUpdated() {
-        Maunbot maunbotSpy = spy(maunbot);
         String validInstruction = "@MaunHouse do stuff.";
         when(status.getText()).thenReturn(validInstruction);
 
-        twitterInstructionSource.addObserver(maunbotSpy);
+        twitterInstructionSource.addObserver(maunbot);
         twitterInstructionSource.onStatus(status);
-        verify(maunbotSpy, times(1)).update(any(TwitterInstructionSource.class), any(Instruction.class));
+        verify(maunbot, times(1)).update(any(TwitterInstructionSource.class), any(Instruction.class));
     }
 
     @Test
     public void onStatus_withInvalidInstruction_observerIsNotUpdated() {
-        Maunbot maunbotSpy = spy(maunbot);
         String invalidInstruction = "Yo, buddy... do stuff.";
         when(status.getText()).thenReturn(invalidInstruction);
 
-        twitterInstructionSource.addObserver(maunbotSpy);
+        twitterInstructionSource.addObserver(maunbot);
         twitterInstructionSource.onStatus(status);
-        verify(maunbotSpy, times(0)).update(any(TwitterInstructionSource.class), any(Instruction.class));
+        verify(maunbot, times(0)).update(any(TwitterInstructionSource.class), any(Instruction.class));
     }
 }
